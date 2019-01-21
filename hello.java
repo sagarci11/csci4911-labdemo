6c9a63805d8571ac949ff47989193d6969cb6986
greenprotein <garciasa@onid.oregonstate.edu>
2019-01-20 22:32:37 -1000

    Added method

--- hello.java
+++ hello.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 class hello {
+    
+    
 
     public static void main(String args[]){
-        System.out.println("hello");
+        //System.out.println("hello");
+        greeting("hello");
     }
 
-
+    public static void greeting2(){
+        Scanner input = new Scanner (System.in);
+        String msg = input.nextLine();
+        System.out.println(msg);
+        input.close();
+        
+    }
+    public static void greeting(String msg){
+        System.out.println(msg);
+    }
 }
